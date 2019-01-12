56646416abbf9555328697433118e5d452652eaf
Thiago Novato <thiagonovato@gmail.com>
2019-01-12 01:53:35 -0200

    Consumindo API e listando na tela o retorno do REST.

--- src/pages/Livros.js
+++ src/pages/Livros.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import api from '../services/api';
 
-export default class Livros extends Component {
+class Livros extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            livros: [],
+            isLoading: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        api
+            .get('livros')
+            .then((res) => {
+                const objetos = Object
+                    .keys(res.data.books)
+                    .map(key => {
+                        return {
+                            id: key,
+                            ...res.data.books[key]
+                        }
+                    })
+
+                this.setState({
+                    isLoading: false,
+                    livros: objetos
+                })
+            })
+
+    }
+
     render() {
         return (
-            <div>Teste</div>
+            <div>
+                {this.state.isLoading && <h1>Carregando...</h1>}
+                {!this.state.isLoading &&
+                    <div>
+                        {this.state.livros.map(livro => <h1 key={livro.id}>{livro.author}</h1>)}
+                    </div>
+                }
+            </div>
         )
     }
 }
+
+export default Livros;
